dfc5d7e7c6dea5c81f1c25537198f299cb071e29
ShivamKumarJhaSuccessive <shivam.jha@successive.tech>
2021-08-26 17:08:33 +0530

    feature patterns

---
+++ extra/patterns/diamond.js
@@ -0,0 +1,32 @@
+function createDimondShape(size){
+    for(var i=1;i<=size;i++){
+       for(var s=size-1;s>=i;s--){
+          process.stdout.write(" ");
+       }
+       for(var j=1;j<=i;j++){
+          process.stdout.write("* ")
+       }
+       console.log();
+    }
+    if(i == size+1){
+        for(var a=1; a<=1; a++){
+            for(var b=1; b<=size; b++){
+                process.stdout.write("* ")
+            }
+        }
+        console.log();
+    }
+    if(i==size+1){
+       for(var i=1;i<=size-1;i++){
+          for(var s=1;s<=i;s++){
+             process.stdout.write(" ");
+          }
+          for(j=i;j<=size-1;j++){
+             process.stdout.write("* ");
+          }
+          console.log();
+       }
+    }
+  }
+  const size = process.argv.slice(2);
+  createDimondShape(Number(size));
