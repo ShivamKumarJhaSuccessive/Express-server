2b66c9e81f9fe5601aa738def01d970ee594d6a5
ShivamKumarJhaSuccessive <shivam.jha@successive.tech>
2021-09-03 13:44:04 +0530

    working validation

---
+++ extraTs/patterns/equilateral.ts
@@ -0,0 +1,14 @@
+let str: string = '';
+const equilateral = (size: number): void => {
+    for (let i: number = 1; i <= size; i++) {
+        for (let s: number = size - 1; s >= i; s--) {
+           str += ' ';
+        }
+        for (let j: number = 1; j <= i; j++) {
+           str += '* ';
+        }
+        str += '\n';
+     }
+     console.log(str);
+};
+export default equilateral;
