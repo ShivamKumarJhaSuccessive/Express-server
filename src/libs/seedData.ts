6087cbeb0ecae4840b606992d52c3d2f2806e50d
ShivamKumarJhaSuccessive <shivam.jha@successive.tech>
2021-09-22 11:42:27 +0530

    working

---
+++ src/libs/seedData.ts
@@ -0,0 +1,29 @@
+import UserRepository from '../repositories/user/UserRepository';
+
+const userRepository: UserRepository = new UserRepository();
+export default () => {
+    userRepository.count()
+        .then(res => {
+            console.log('res',res);
+
+            if (res === 0) {
+                console.log('Data seeding in progress');
+                userRepository.create(
+                    {
+                     name : 'Head-Trainer' , 
+                     role: 'head-trainer', 
+                     email: 'headtrainer@successive.tech', 
+                     password: 'Training@123'
+                    }
+                );
+                userRepository.create(
+                    {
+                     name : 'Trainer' , 
+                     role: 'trainer', 
+                     email: 'trainer@successive.tech', 
+                     password: 'Training@123'
+                    }
+                );
+            }
+        }).catch(err => (console.log(err)));
+};
