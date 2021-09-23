58a8bbef57d471ce2a76c04e0d42262b7069e5ef
ShivamKumarJhaSuccessive <shivam.jha@successive.tech>
2021-09-23 16:29:06 +0530

    working

--- src/controllers/trainee/controller.ts
+++ src/controllers/trainee/controller.ts
@@ -1,4 +1,4 @@
-import { userRepository, BCRYPT_SALT_ROUNDS } from './../../libs/seedData';
+import { traineeRepository, BCRYPT_SALT_ROUNDS } from './../../libs/seedData';
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
 import configuration from '../../config/configuration';
@@ -8,15 +8,33 @@ import * as bcrypt from 'bcrypt'
 
 class trainees{
     async get(req:Request, res:Response, next: NextFunction){
-        const userData = await userRepository.findAll({});
-        return res.status(200).send({message: 'Fetched data Succesfully',data: userData});
+        try {
+            const limit = Math.abs(JSON.parse(req.query.limit as string));
+            const skip = Math.abs(JSON.parse(req.query.skip as string));
+            const sortByDate = { createdAt: req.query.sort };
+            const sortByName = { name: req.query.name };
+            const sortByEmail = { email: req.query.email };
+            const searchQuery = {
+              $or: [
+                { name: { $regex: new RegExp(JSON.stringify(req.query.search)), $options: 'i' } },
+                { email: { $regex: new RegExp(JSON.stringify(req.query.search)), $options: 'i' } },
+              ],
+            };
+            const role = { role: 'head-trainer' };
+            const data1 = await traineeRepository.findAll(searchQuery).select(role).limit(limit).skip(skip).sort([[sortByDate], [sortByName], [sortByEmail]]);
+            const allTraineesCount = await traineeRepository.countDoc({});
+            const dataAll = [{ Total_Trainees_in_db: allTraineesCount, Total_Trainees_from_query: data1 }];
+            return res.status(200).send({ message: 'All Trainees received.', data: dataAll, status: 'success' });
+          } catch (err) {
+            return res.status(500).json({ message: err.message });
+          }
         
     }
     async post( req: Request, res: Response, next: NextFunction){
         const {name,email,password} = req.body;
         const passwordHash = await bcrypt.hash(password, BCRYPT_SALT_ROUNDS);
         const newUser = { name,email, password: passwordHash };
-        const userData = await userRepository.createDoc(newUser);
+        const userData = await traineeRepository.createDoc(newUser);
         return res.status(200).send({message:'trainee added succesfully',data:{userData}});
     }
     createToken(req: Request, res: Response, next: NextFunction) {
--- src/libs/seedData.ts
+++ src/libs/seedData.ts
@@ -1,11 +1,14 @@
 import UserRepository from '../repositories/user/UserRepository';
 import * as bcrypt from 'bcrypt' 
 import Configure from '../config/configuration';
+import TraineeRepository from '../repositories/trainee/TraineeRepository';
 export const BCRYPT_SALT_ROUNDS: number = 10;
 
 export const userRepository: UserRepository = new UserRepository();
+export const traineeRepository: TraineeRepository= new TraineeRepository();
 export default async () => {
-    const res = await userRepository.count()
+    let res
+     res = await userRepository.count()
         try {
             console.log('res',res);
 
@@ -15,13 +18,41 @@ export default async () => {
 
                 userRepository.create(
                     {
+                     name : 'Trainee' , 
+                     role: 'Trainee', 
+                     email: 'Trainee@successive.tech', 
+                     password: hash
+                    }
+                );
+                userRepository.create(
+                    {
+                     name : 'Trainee2' , 
+                     role: 'trainee', 
+                     email: 'Trainee@successive.tech', 
+                     password: hash
+                    }
+                );
+            }   
+        } catch (error) {
+          console.log('error',error)  
+        }
+        res = await traineeRepository.count()
+        try {
+            console.log('res',res);
+
+            if (res === 0) {
+                console.log('Data seeding in progress');
+                const hash = await bcrypt.hash(Configure.password,BCRYPT_SALT_ROUNDS)
+
+                traineeRepository.create(
+                    {
                      name : 'Head-Trainer' , 
                      role: 'head-trainer', 
                      email: 'headtrainer@successive.tech', 
                      password: hash
                     }
                 );
-                userRepository.create(
+                traineeRepository.create(
                     {
                      name : 'Trainer' , 
                      role: 'trainer', 
